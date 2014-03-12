9a44273e67512a936971d8693a0c8caf1ff040c3
triffe <triffe@demog.berkeley.edu>
2014-03-12 08:59:37 -0700

    made easier to pull from XXX_Old

--- IDButils/IDButils/R/readIDBcurrent.R
+++ IDButils/IDButils/R/readIDBcurrent.R
@@ -5,6 +5,7 @@
 #' 
 #' @param XXX character string; HMD country short code
 #' @param item character string; one of \code{c("death","pop","birth","tadj","monthly")}
+#' @param old logical should the file come from XXX_Old?
 #' @param full.path optional. *If you're not at UCB, or if you're no working from the main HMD working directory \code{"~/HMDWORK"}, then give the full path to the file, including \code{".txt"}.
 #' 
 #' @export
@@ -15,12 +16,14 @@
 #' #bo <- readIDBcurrent("SWE","birth")
 #' #bo <- resortBirths(bo)
 #' 
-readIDBcurrent <- function(XXX = "SWE", item = "death", full.path = NULL){
+readIDBcurrent <- function(XXX = "SWE", item = "death", old = FALSE, full.path = NULL){
   
   stopifnot(item %in% c(c("death","pop","birth","tadj","monthly")))
   
   this.path <- ifelse(is.null(full.path), 
-                      file.path("~/HMDWORK", XXX, "InputDB", paste0(XXX, item, ".txt")), 
+                      ifelse(old,
+                        file.path("~/HMDWORK", paste0(XXX,"_Old"), "InputDB", paste0(XXX, item, ".txt")), 
+                        file.path("~/HMDWORK", XXX, "InputDB", paste0(XXX, item, ".txt"))), 
                       full.path)
   
   obj <- read.table(this.path, 
