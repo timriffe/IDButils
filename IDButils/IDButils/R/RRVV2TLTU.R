12b8df5576a31295208806377c939e18650a174b
triffe <triffe@demog.berkeley.edu>
2014-10-23 15:40:05 -0700

    added function for splitting RR and VV to triangles

---
+++ IDButils/IDButils/R/RRVV2TLTU.R
@@ -0,0 +1,21 @@
+
+#'@title RRVV2TLTU split RR and VV redundant tabulations into TL and TU Lexis shapes
+#' 
+#' @description This takes advantage of the fact that age 0 VV is actually a TL, and then iterates upward. There can be no gaps in ages, and ages must be single.
+#' RR and VV must be the same length.
+#' 
+#' @param RR a vector of age-period count data, in in single ages, including the open age group, not including TOT or UNK. (demo_magec, if EUROSTAT is the source)
+#' @param VV a vector of the same length as RR for count data in vertical parallelograms, i.e. 'age reached during the year'. excl TOT and UNK (demo_mager, if EUROSTAT is the source)
+
+#' @return a list containing elements TL and TU. TL is 1 element longer than TU
+#' 
+#' @export 
+RRVV2TLTU <- function(RR, VV){
+  # assuming ages organized in same way
+  # no UNK and no TOT, last age open
+  n  <- length(RR)
+  TU <- cumsum(RR - VV)
+  TL <- c((RR - TU)[-n], RR[n])
+  list(TL = TL, TU = TU)
+}
+
